1e216ecf1fb6b80e2af930cebd125fc880cbc5e3
Dorra-10 <161203207+Dorra-10@users.noreply.github.com>
2024-08-20 16:59:02 +0100

    Add files via upload

--- task1.js
+++ task1.js
@@ -2,11 +2,13 @@ var FullCon = document.getElementById("FullCon");
 var Full = document.getElementById("Full");
 var currentInd = 0;
 var images = document.querySelectorAll('.Gal img');
+
 function openFull(index) {
     currentInd = index;
     FullCon.style.display = "flex";
     Full.src = images[currentInd].src;
 }
+
 function closeFull() {
     FullCon.style.display = "none";
 }
@@ -23,15 +25,16 @@ function prevImg() {
 
 document.getElementById('searchB').addEventListener('click', () => {
     const query = document.getElementById('search').value.toLowerCase();
-    let Found = false;
-    images.forEach((image , index) => {
+    let found = false;
+    images.forEach((image, index) => {
         const keywords = image.getAttribute('data-keywords').toLowerCase();
-        if (keywords.includes(query) && query !== '') {
-             Found = true ;
-             openFull(index);
-        } 
+        if (keywords === query) {
+            found = true;
+            openFull(index);
+        }
     });
-    if(Found == false){
+
+    if (!found) {
         alert("No matching image found");
     }
 });
